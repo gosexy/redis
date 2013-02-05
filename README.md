124ff2ad1c1346e732bf823557c0af5fb17e6867
José Carlos Nieto <xiam@menteslibres.org>
2013-02-04 22:35:57 -0600

    Adding functions for some Redis commands and a README.md file.

---
+++ README.md
@@ -0,0 +1,10 @@
+# gosexy/redis
+
+A Go wrapper for the official [Redis][1] client, [hiredis][2].
+
+## How to install
+
+Don't. This is currently a work in progress.
+
+[1]: http://redis.io
+[2]: https://github.com/redis/hiredis
--- main.go
+++ main.go
@@ -74,6 +74,10 @@ func New() *Client {
 	return self
 }
 
+func byteValue(v interface{}) []byte {
+	return []byte(fmt.Sprintf("%v", v))
+}
+
 func (self *Client) ConnectWithTimeout(host string, port uint, timeout time.Duration) error {
 	var ctx *C.redisContext
 
@@ -274,15 +278,6 @@ func (self *Client) command(dest interface{}, values ...[]byte) error {
 	return setReplyValue(rv.Elem(), reply)
 }
 
-func (self *Client) Del(name string) (int64, error) {
-	var s int64
-	err := self.command(&s,
-		[]byte(string("DEL")),
-		[]byte(string(name)),
-	)
-	return s, err
-}
-
 func (self *Client) LRange(name string, min int, max int) ([]string, error) {
 	var ret []string
 	err := self.command(&ret,
@@ -292,15 +287,6 @@ func (self *Client) LRange(name string, min int, max int) ([]string, error) {
 		[]byte(strconv.Itoa(max)),
 	)
 	return ret, err
-}
-
-func (self *Client) Get(name string) (string, error) {
-	var s string
-	err := self.command(&s,
-		[]byte(string("GET")),
-		[]byte(string(name)),
-	)
-	return s, err
 }
 
 func (self *Client) Incr(name string) (int64, error) {
@@ -322,12 +308,12 @@ func (self *Client) Set(name string, value string) (string, error) {
 	return s, err
 }
 
-func (self *Client) LPush(name string, value string) (int64, error) {
+func (self *Client) LPush(name string, value interface{}) (int64, error) {
 	var ret int64
 	err := self.command(&ret,
 		[]byte(string("LPUSH")),
 		[]byte(string(name)),
-		[]byte(string(value)),
+		byteValue(value),
 	)
 	return ret, err
 }
@@ -336,4 +322,588 @@ func (self *Client) Ping() (string, error) {
 	var s string
 	err := self.command(&s, []byte("PING"))
 	return s, err
+}
+
+/*
+If key already exists and is a string, this command appends the value at the
+end of the string. If key does not exist it is created and set as an empty
+string, so APPEND will be similar to SET in this special case.
+
+http://redis.io/commands/append
+*/
+func (self *Client) Append(key string, value interface{}) (int64, error) {
+	var ret int64
+	err := self.command(
+		&ret,
+		[]byte("APPEND"),
+		[]byte(key),
+		byteValue(value),
+	)
+	return ret, err
+}
+
+/*
+Request for authentication in a password-protected Redis server. Redis can be
+instructed to require a password before allowing clients to execute commands.
+This is done using the requirepass directive in the configuration file.
+
+http://redis.io/commands/auth
+*/
+func (self *Client) Auth(password string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("AUTH"),
+		[]byte(password),
+	)
+	return ret, err
+}
+
+/*
+Instruct Redis to start an Append Only File rewrite process. The rewrite will
+create a small optimized version of the current Append Only File.
+
+http://redis.io/commands/bgwriteaof
+*/
+func (self *Client) BgRewriteAOF() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("BGREWRITEAOF"),
+	)
+	return ret, err
+}
+
+/*
+Save the DB in background. The OK code is immediately returned. Redis forks, the
+parent continues to serve the clients, the child saves the DB on disk then exits.
+
+A client my be able to check if the operation succeeded using the LASTSAVE
+command.
+
+http://redis.io/commands/bgsave
+*/
+func (self *Client) BgSave() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("BGSAVE"),
+	)
+	return ret, err
+}
+
+/*
+Count the number of set bits (population counting) in a string.
+
+http://redis.io/commands/bitcount
+*/
+func (self *Client) BitCount(key string, start int64, end int64) (int64, error) {
+	var ret int64
+	err := self.command(
+		&ret,
+		[]byte("BITCOUNT"),
+		byteValue(start),
+		byteValue(end),
+	)
+	return ret, err
+}
+
+/*
+Perform a bitwise operation between multiple keys (containing string values)
+and store the result in the destination key.
+
+http://redis.io/commands/bitop
+*/
+func (self *Client) BitOp(op string, keys ...string) (int64, error) {
+	var ret int64
+	args := make([][]byte, len(keys)+1)
+	args[0] = []byte("BITOP")
+	for i, key := range keys {
+		args[1+i] = byteValue(key)
+	}
+	err := self.command(ret, args...)
+	return ret, err
+}
+
+/*
+BLPOP is a blocking list pop primitive. It is the blocking version of LPOP
+because it blocks the connection when there are no elements to pop from any of
+the given lists. An element is popped from the head of the first list that is
+non-empty, with the given keys being checked in the order that they are given.
+
+http://redis.io/commands/blpop
+*/
+func (self *Client) BlPop(timeout uint64, keys ...string) ([]string, error) {
+	var i int
+	var ret []string
+	args := make([][]byte, len(keys)+2)
+	args[0] = []byte("BLPOP")
+
+	for i, key := range keys {
+		args[1+i] = byteValue(key)
+	}
+	args[1+i] = byteValue(timeout)
+
+	err := self.command(ret, args...)
+	return ret, err
+}
+
+/*
+BRPOP is a blocking list pop primitive. It is the blocking version of RPOP
+because it blocks the connection when there are no elements to pop from any of
+the given lists. An element is popped from the tail of the first list that is
+non-empty, with the given keys being checked in the order that they are given.
+
+http://redis.io/commands/brpop
+*/
+func (self *Client) BrPop(timeout uint64, keys ...string) ([]string, error) {
+	var i int
+	var ret []string
+
+	args := make([][]byte, len(keys)+2)
+	args[0] = []byte("BRPOP")
+
+	for i, key := range keys {
+		args[1+i] = byteValue(key)
+	}
+	args[1+i] = byteValue(timeout)
+
+	err := self.command(ret, args...)
+	return ret, err
+}
+
+/*
+BRPOPLPUSH is the blocking variant of RPOPLPUSH. When source contains elements,
+this command behaves exactly like RPOPLPUSH. When source is empty, Redis will
+block the connection until another client pushes to it or until timeout is
+reached. A timeout of zero can be used to block indefinitely.
+
+http://redis.io/commands/brpoplpush
+*/
+func (self *Client) BrPopLPush(source string, destination string, timeout int64) ([]string, error) {
+	var ret []string
+	err := self.command(
+		&ret,
+		[]byte("BRPOPLPUSH"),
+		byteValue(source),
+		byteValue(destination),
+		byteValue(timeout),
+	)
+	return ret, err
+}
+
+/*
+The CLIENT KILL command closes a given client connection identified by ip:port.
+
+http://redis.io/commands/client-kill
+*/
+func (self *Client) ClientKill(ip string, port uint) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CLIENT KILL"),
+		byteValue(fmt.Sprintf("%s:%d", ip, port)),
+	)
+	return ret, err
+}
+
+/*
+The CLIENT LIST command returns information and statistics about the client
+connections server in a mostly human readable format.
+
+http://redis.io/commands/client-list
+*/
+func (self *Client) ClientList() ([]string, error) {
+	var ret []string
+	err := self.command(
+		&ret,
+		[]byte("CLIENT LIST"),
+	)
+	return ret, err
+}
+
+/*
+The CLIENT GETNAME returns the name of the current connection as set by CLIENT
+SETNAME. Since every new connection starts without an associated name, if no
+name was assigned a null bulk reply is returned.
+
+http://redis.io/commands/client-getname
+*/
+func (self *Client) ClientGetName() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CLIENT GETNAME"),
+	)
+	return ret, err
+}
+
+/*
+The CLIENT SETNAME command assigns a name to the current connection.
+
+http://redis.io/commands/client-setname
+*/
+func (self *Client) ClientSetName(connectionName string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CLIENT SETNAME"),
+		byteValue(connectionName),
+	)
+	return ret, err
+}
+
+/*
+The CONFIG GET command is used to read the configuration parameters of a running
+Redis server. Not all the configuration parameters are supported in Redis 2.4,
+while Redis 2.6 can read the whole configuration of a server using this command.
+
+http://redis.io/commands/config-get
+*/
+func (self *Client) ConfigGet(parameter string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CONFIG GET"),
+		byteValue(parameter),
+	)
+	return ret, err
+}
+
+/*
+The CONFIG SET command is used in order to reconfigure the server at run time
+without the need to restart Redis. You can change both trivial parameters or
+switch from one to another persistence option using this command.
+
+http://redis.io/commands/config-set
+*/
+func (self *Client) ConfigSet(parameter string, value interface{}) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CONFIG SET"),
+		byteValue(parameter),
+		byteValue(value),
+	)
+	return ret, err
+}
+
+/*
+Resets the statistics reported by Redis using the INFO command.
+
+http://redis.io/commands/config-resetstat
+*/
+func (self *Client) ConfigResetStat() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("CONFIG RESETSTAT"),
+	)
+	return ret, err
+}
+
+/*
+Return the number of keys in the currently-selected database.
+
+http://redis.io/commands/dbsize
+*/
+func (self *Client) DbSize() (uint64, error) {
+	var ret uint64
+	err := self.command(
+		&ret,
+		[]byte("DBSIZE"),
+	)
+	return ret, err
+}
+
+/*
+DEBUG OBJECT is a debugging command that should not be used by clients. Check
+the OBJECT command instead.
+
+http://redis.io/commands/debug-object
+*/
+func (self *Client) DebugObject(key string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("DEBUG OBJECT"),
+		byteValue(key),
+	)
+	return ret, err
+}
+
+/*
+DEBUG SEGFAULT performs an invalid memory access that crashes Redis. It is used
+to simulate bugs during the development.
+
+http://redis.io/commands/debug-segfault
+*/
+func (self *Client) DebugSegfault() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("DEBUG SEGFAULT"),
+	)
+	return ret, err
+}
+
+/*
+Decrements the number stored at key by one. If the key does not exist, it is set
+to 0 before performing the operation. An error is returned if the key contains a
+value of the wrong type or contains a string that can not be represented as
+integer. This operation is limited to 64 bit signed integers.
+
+http://redis.io/commands/decr
+*/
+func (self *Client) Decr(key string) (int64, error) {
+	var ret int64
+	err := self.command(
+		&ret,
+		[]byte("DECR"),
+		byteValue(key),
+	)
+	return ret, err
+}
+
+/*
+Decrements the number stored at key by decrement. If the key does not exist, it
+is set to 0 before performing the operation. An error is returned if the key
+contains a value of the wrong type or contains a string that can not be
+represented as integer. This operation is limited to 64 bit signed integers.
+
+http://redis.io/commands/decrby
+*/
+func (self *Client) DecrBy(key string, decrement int64) (int64, error) {
+	var ret int64
+	err := self.command(
+		&ret,
+		[]byte("DECRBY"),
+		byteValue(key),
+		byteValue(decrement),
+	)
+	return ret, err
+}
+
+/*
+Removes the specified keys. A key is ignored if it does not exist.
+
+http://redis.io/commands/del
+*/
+
+func (self *Client) Del(keys ...string) (int64, error) {
+	var ret int64
+	args := make([][]byte, len(keys)+1)
+	args[0] = []byte("DEL")
+	for i, key := range keys {
+		args[1+i] = byteValue(key)
+	}
+	err := self.command(&ret, args...)
+	return ret, err
+}
+
+/*
+Flushes all previously queued commands in a transaction and restores the
+connection state to normal.
+
+http://redis.io/commands/discard
+*/
+func (self *Client) Discard() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("DISCARD"),
+	)
+	return ret, err
+}
+
+/*
+Serialize the value stored at key in a Redis-specific format and return it to
+the user. The returned value can be synthesized back into a Redis key using the
+RESTORE command.
+
+http://redis.io/commands/dump
+*/
+func (self *Client) Dump(key string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("DUMP"),
+		byteValue(key),
+	)
+	return ret, err
+}
+
+/*
+Returns message.
+
+http://redis.io/commands/echo
+*/
+func (self *Client) Echo(message string) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("ECHO"),
+		byteValue(message),
+	)
+	return ret, err
+}
+
+/*
+Executes all previously queued commands in a transaction and restores the connection state to normal.
+
+http://redis.io/commands/exec
+*/
+func (self *Client) Exec() ([]string, error) {
+	var ret []string
+	err := self.command(
+		&ret,
+		[]byte("EXEC"),
+	)
+	return ret, err
+}
+
+/*
+Returns if key exists.
+
+http://redis.io/commands/exists
+*/
+func (self *Client) Exists(key string) (bool, error) {
+	var ret bool
+	err := self.command(
+		&ret,
+		[]byte("EXISTS"),
+		byteValue(key),
+	)
+	return ret, err
+}
+
+/*
+Set a timeout on key. After the timeout has expired, the key will automatically
+be deleted. A key with an associated timeout is often said to be volatile in
+Redis terminology.
+
+http://redis.io/commands/expire
+*/
+func (self *Client) Expire(key string, seconds uint64) (bool, error) {
+	var ret bool
+	err := self.command(
+		&ret,
+		[]byte("EXPIRE"),
+		byteValue(key),
+		byteValue(seconds),
+	)
+	return ret, err
+}
+
+/*
+EXPIREAT has the same effect and semantic as EXPIRE, but instead of specifying
+the number of seconds representing the TTL (time to live), it takes an absolute
+Unix timestamp (seconds since January 1, 1970).
+
+http://redis.io/commands/expireat
+*/
+func (self *Client) ExpireAt(key string, unixTime uint64) (bool, error) {
+	var ret bool
+	err := self.command(
+		&ret,
+		[]byte("EXPIREAT"),
+		byteValue(key),
+		byteValue(unixTime),
+	)
+	return ret, err
+}
+
+/*
+Delete all the keys of all the existing databases, not just the currently selected one. This command never fails.
+
+http://redis.io/commands/flushall
+*/
+func (self *Client) FlushAll() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("FLUSHALL"),
+	)
+	return ret, err
+}
+
+/*
+Delete all the keys of the currently selected DB. This command never fails.
+
+http://redis.io/commands/flushdb
+*/
+func (self *Client) FlushDb() (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte("FLUSHDB"),
+	)
+	return ret, err
+}
+
+/*
+Get the value of key. If the key does not exist the special value nil is
+returned. An error is returned if the value stored at key is not a string,
+because GET only handles string values.
+
+http://redis.io/commands/get
+*/
+func (self *Client) Get(key string) (string, error) {
+	var s string
+	err := self.command(&s,
+		[]byte(string("GET")),
+		byteValue(key),
+	)
+	return s, err
+}
+
+/*
+Returns the bit value at offset in the string value stored at key.
+
+http://redis.io/commands/getbit
+*/
+func (self *Client) GetBit(key string, offset int64) (int64, error) {
+	var s int64
+	err := self.command(&s,
+		[]byte(string("GETBIT")),
+		byteValue(key),
+		byteValue(offset),
+	)
+	return s, err
+}
+
+/*
+Returns the substring of the string value stored at key, determined by the
+offsets start and end (both are inclusive). Negative offsets can be used in
+order to provide an offset starting from the end of the string. So -1 means
+the last character, -2 the penultimate and so forth.
+
+http://redis.io/commands/getrange
+*/
+func (self *Client) GetRange(key string, start int64, end int64) ([]string, error) {
+	var ret []string
+	err := self.command(&ret,
+		[]byte("GETRANGE"),
+		byteValue(key),
+		byteValue(start),
+		byteValue(end),
+	)
+	return ret, err
+}
+
+/*
+Atomically sets key to value and returns the old value stored at key. Returns an
+error when key exists but does not hold a string value.
+
+http://redis.io/commands/getset
+*/
+func (self *Client) GetSet(key string, value interface{}) (string, error) {
+	var ret string
+	err := self.command(
+		&ret,
+		[]byte(string("GETSET")),
+		byteValue(key),
+		byteValue(value),
+	)
+	return ret, err
 }
